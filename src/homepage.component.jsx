356d0fcf3d37b66bd10b03fae02d4631e7eb5f8e
Umair Akhtar <umaakhta@publicisgroupe.net>
2020-01-16 14:15:03 +0530

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Hats
+                    </h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Pants
+                    </h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Shirts
+                    </h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Socks
+                    </h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Handkerchief
+                    </h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+)
+
+export default HomePage;
